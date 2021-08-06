c03208953df10f7826ed7ac116e01d9998e49eaf
racheldunne-CO <rachel.dunne@cabinetoffice.gov.uk>
2021-08-06 18:10:10 +0100

    Feat: get working for r = 1

--- count_triplets.py
+++ count_triplets.py
@@ -2,10 +2,16 @@
 # You are given an array and you need to find number of tripets of
 # indices (i, j, k) such that the elements at those indices are
 # in geometric progression for a given common ratio  and i < j < k
-
+import itertools
 
 def countTriplets(arr, r):
-    triplets_list_unique = [(n, r * n, r * r * n) for n in arr if r * n in arr and r * r * n in arr]
-    extra_elements = [t for t in triplets_list_unique for i in range((arr.count(t[1]) - 1) + (arr.count(t[2]) - 1))]
-    triplets_list = triplets_list_unique + extra_elements
+    if r > 1:
+        triplets_list_short = [(n, r * n, r * r * n) for n in arr if r * n in arr and r * r * n in arr]
+        extra_elements = [t for t in triplets_list_short for i in range((arr.count(t[1]) - 1) + (arr.count(t[2]) - 1))]
+        triplets_list = triplets_list_short + extra_elements
+    elif r == 1:
+        same_number = {
+            k: [k] * arr.count(k) for k in arr
+        }
+        triplets_list = [t for k in same_number for t in itertools.combinations(same_number[k], 3)]
     return len(triplets_list)
