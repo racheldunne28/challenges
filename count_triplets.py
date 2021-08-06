8bf06893b3baa985d7a20b13f62a2dd77d8b5a06
racheldunne-CO <rachel.dunne@cabinetoffice.gov.uk>
2021-08-06 08:11:25 +0100

    linting

--- count_triplets.py
+++ count_triplets.py
@@ -1,9 +1,10 @@
 # https://www.hackerrank.com/challenges/count-triplets-1/problem?fbclid=IwAR2iS6zOE0VJLGrGXl_o07trE1aS3feESNOK68VLu0YqzhoviSSAvi5tbao
-# You are given an array and you need to find number of tripets of indices (i, j, k) such that the elements at those indices are
+# You are given an array and you need to find number of tripets of
+# indices (i, j, k) such that the elements at those indices are
 # in geometric progression for a given common ratio  and i < j < k
 
 
 def countTriplets(arr, r):
-    l = [(n, r * n, r * r * n) for n in arr if r * n in arr and r * r * n in arr]
-    s = set(l)
-    return len(s)
+    triplets_list = [(n, r * n, r * r * n) for n in arr if r * n in arr and r * r * n in arr]
+    triplets_set = set(triplets_list)
+    return len(triplets_set)
